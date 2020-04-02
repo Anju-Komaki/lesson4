536a30b1798d81bc207b2e8a417f326b4cf78775
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-04-02 04:11:58 +0000

    [modified]setting root

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
   def create
   	book = Book.new(book_params)
   	book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path
   end
 
   def edit
@@ -23,12 +23,12 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to book_path
   end
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to new_book_path
+    redirect_to new_book_path, notice: "Book was successfully destoyed."
   end
 
   private
