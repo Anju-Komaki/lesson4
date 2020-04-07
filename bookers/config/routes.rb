9b26865dd451a017609c499d8cb70a32831943a2
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-04-07 06:31:57 +0000

    [finish]4-8

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 root :to => 'books#index'
 get 'books' => 'books#new' ,as:'books'
-get 'books/:id' => 'books#show', as:'show_book'
+get 'books/:id' => 'books#show', as:'book'
 delete 'books/:id' => 'books#destroy', as:'destroy_book'
 get 'books/:id/edit' => 'books#edit', as: 'edit_book'
 resources :books
