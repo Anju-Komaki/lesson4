facf5f1f74f3048ce57836a53afbca569e5c1c47
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-04-08 05:18:57 +0000

    [finish]5-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6f445fb3ecdc10ffca09f4f6c074777a452c640cd6b1137d8a23112aa9b2a29abff8010df956cc6e78b5cbb94a579635f017d7a30d3d56196bd8540f9d7dfc2b'
