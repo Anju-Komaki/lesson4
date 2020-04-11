bc800e1d7a5296ab4ece06e4de1d7343e347fb72
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-04-11 02:27:27 +0000

    [modified]user columnname

--- sbookers/app/controllers/application_controller.rb
+++ sbookers/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
 	  protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :email, :password, :password_confirmation])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:user_name, :password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :password])
   end
 end
