621cbe84b3d8480968f3008eae6b5f458a3f31b2
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-04-12 12:00:23 +0000

    [modified]index

--- sbookers/config/initializers/application_controller_renderer.rb
+++ sbookers/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2bbc0f3751fc713da52f82d15086a0b149259584a43e859c91bdd033af09b712367bb46965b49fa98dab05ccddba8fc9512365750a6aba270e38c2b02a30d0cf'
