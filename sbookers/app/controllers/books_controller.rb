5b99a0063f026e51f9c80ff90d60edd6d3712f62
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-04-14 02:19:24 +0000

    [add]book validates

--- sbookers/app/controllers/books_controller.rb
+++ sbookers/app/controllers/books_controller.rb
@@ -1,20 +1,37 @@
 class BooksController < ApplicationController
+	
 	def create
 		@book = Book.new(book_params)
 		@book.user_id = current_user.id
-		@book.save
+	 if @book.save
+	 	flash[:success] = "You have creatad book successfully."
 		redirect_to book_path(@book)
+     else
+     	@books = Book.all
+	    render :new
+     end
 	end
+
 	def new
-		@book = Book.new
 		@books = Book.all
+		@book = Book.new
 	end
+
 	def show
 		@book = Book.find(params[:id])
+	 if @book.save
+	 	flash[:success] = "You have creatad book successfully."
+		redirect_to book_path(@book)
+     else
+     	@books = Book.all
+	    render :new
+	 end
 	end
+
 	def edit
 		@book = Book.find(params[:id])
 	end
+
 	def update
 	    @book = Book.find(params[:id])
 	 if @book.update(book_params)
@@ -24,11 +41,13 @@ class BooksController < ApplicationController
       render :edit
      end
 	end
+
 	def destroy
 		@book = Book.find(params[:id])
 		@book.destroy
 		redirect_to new_book_path
 	end
+
 	private
 	def book_params
 		params.require(:book).permit(:title, :body)
