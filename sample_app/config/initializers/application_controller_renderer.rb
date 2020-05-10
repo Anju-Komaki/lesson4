31e1250717397d5e4dcc7afc274cb8c552c61cdb
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-05-10 11:49:52 +0000

    first commit

---
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '7cd1f5a11c09ddaf27696a656f827de69bd3e86feed7b2d3e1fcd0716cc6f11c96a81a773dc9b2b4ed927863d46ccad48b7108dbb827c0e7188fbae5870f3038'
