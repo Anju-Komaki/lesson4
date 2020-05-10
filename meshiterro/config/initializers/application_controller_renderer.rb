0051b94b1ef30972dbfd1a327f06cdd7fce39259
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-05-10 09:28:05 +0000

    first commit

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '6f445fb3ecdc10ffca09f4f6c074777a452c640cd6b1137d8a23112aa9b2a29abff8010df956cc6e78b5cbb94a579635f017d7a30d3d56196bd8540f9d7dfc2b'
